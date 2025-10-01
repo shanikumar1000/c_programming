77fcacaf6dfb8cc73ac8d3e0e4619fd885a7461e
Shanikumar Nanaware <shanikumarnanaware@gmail.com>
2025-10-01 14:34:48 +0530

    Print the size of data type

---
+++ scanning/15_sizeof.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("The size of char data type is : %zu bytes\n", sizeof(char));
+    printf("The size of int data type is : %zu bytes\n", sizeof(int));
+    printf("The size of float data type is : %zu bytes\n", sizeof(float));
+    printf("The size of double data type is : %zu bytes\n", sizeof(double));
+    return 0;
+}
