3626f81b86fc7f202714b0b45ab91ca5894b34a5
Shanikumar Nanaware <shanikumarnanaware@gmail.com>
2025-10-03 15:22:13 +0530

    First 20 whole numbers.

---
+++ scanning/50_whole_numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    printf("The first 20 whole numbers are:\n");
+    for (i = 0; i <= 19; i++)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}
